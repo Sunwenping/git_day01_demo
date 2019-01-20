68c87c8b7e33908fb4b55d25e8052c22b41b2187
sunwenping <sunwenping>
2019-01-20 20:58:16 +0800

    提交

--- git_day01_demo/src/main/java/com/ithiem/domain/User.java
+++ git_day01_demo/src/main/java/com/ithiem/domain/User.java
@@ -3,5 +3,6 @@ package com.ithiem.domain;
 public class User {
     public static void main(String[] args) {
         System.out.println("helloWold");
+        System.out.println("hahah ");
     }
 }
