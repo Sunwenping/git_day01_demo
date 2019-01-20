5cb21fbc1edb46f3cce25d424a4d9e67c7e418ce
sunwenping <sunwenping>
2019-01-20 21:02:34 +0800

    提交

--- git_day01_demo/src/main/java/com/ithiem/domain/User.java
+++ git_day01_demo/src/main/java/com/ithiem/domain/User.java
@@ -1,8 +1,6 @@
 package com.ithiem.domain;
 
 public class User {
-    public static void main(String[] args) {
-        System.out.println("helloWold");
-        System.out.println("hahah ");
-    }
+   private String name;
+   private Integer id;
 }
