6fc49091800f7844608c7de1c9fedbed5be30e37
sunwenping <sunwenping>
2019-03-24 15:09:57 +0800

    第一次修改

--- git_day01_demo/src/main/java/com/ithiem/domain/User.java
+++ git_day01_demo/src/main/java/com/ithiem/domain/User.java
@@ -4,4 +4,12 @@ public class User {
    private String name;
    private Integer id;
    private String sex;
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
 }
